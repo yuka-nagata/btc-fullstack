3499907dcab9fdc8f4d3f23ff1c909c3056887a2
yuka-matsuki <yuka_nagata@mail.toyota.co.jp>
2025-11-19 00:16:16 +0900

    knexfileの修正

--- db/knexfile.js
+++ db/knexfile.js
@@ -16,4 +16,14 @@ module.exports = {
     },
     seeds: { directory: "./data/seeds" },
   },
+  production: {
+    client: "pg",
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: { directory: "./data/seeds" },
+  },
 };
